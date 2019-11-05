7fb48a6ed5fddc57e377b37aaa65d0e62dad0adf
Evgeny Efremov <efremmof@yandex.ru>
2019-11-05 19:25:05 +0300

    context contacts

--- mainapp/views.py
+++ mainapp/views.py
@@ -16,7 +16,25 @@ def catalog(request):
 
 
 def contacts(request):
+    contacts = [
+        {
+            'address': 'Кировская область, г. Киров, ул.Книжная, дом 77',
+            'phone': '+7(777) 777-77-77',
+            'email': 'kirovbooks@mail.ru',
+        },
+        {
+            'address': 'Кировская область, г. Кирово-Чепецк, ул.Цветочная, дом 55',
+            'phone': '+7(555) 555-55-55',
+            'email': 'chepetskbooks@mail.ru',
+        },
+        {
+            'address': 'Кировская область, г. Слободской, ул.Энтузиастов, дом 33',
+            'phone': '+7(333) 333-33-33',
+            'email': 'slobodabooks@mail.ru',
+        },
+    ]
     context = {
         'page_title': 'контакты',
+        'contacts': contacts,
     }
     return render(request, 'mainapp/contacts.html', context)
