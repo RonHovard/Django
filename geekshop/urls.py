16c1cf79630edad44607c48fd4cb6ec4d99e8a38
Evgeny Efremov <efremmof@yandex.ru>
2019-11-05 19:54:38 +0300

    url

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,8 +18,8 @@ from django.urls import path
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.index),
-    path('catalog/', mainapp.catalog),
-    path('contacts/', mainapp.contacts),
+    path('', mainapp.index, name='index'),
+    path('catalog/', mainapp.catalog, name='catalog'),
+    path('contacts/', mainapp.contacts, name='contacts'),
     path('admin/', admin.site.urls),
 ]
