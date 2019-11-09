44e6aead2a658bb1e20293dd726009a957845632
Evgeny Efremov <efremmof@yandex.ru>
2019-11-09 13:04:06 +0300

    makemigrations, migrate, create 4 category in DB

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='категория', max_length=64)
+    description = models.TextField(verbose_name='описание', blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
